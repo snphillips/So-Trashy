04fbef3d0e12867ec37e773d1f6c7a94d798be4c
Sarah Phillips <snphillips@gmail.com>
2026-07-29 20:05:55 -0400

    fix duplicate line in tooltip

--- src/utilities/tooltip.ts
+++ src/utilities/tooltip.ts
@@ -94,7 +94,7 @@ export function generateTooltipHTML(
     <h4>${d.communityDistrictName}</h4>
     ${tooltipYear} population: ${new Intl.NumberFormat().format(getPopulation(d))} <br/>
     neighborhood total: ${new Intl.NumberFormat().format(d[refuseType])} tons/year<br/>
-    per person: ${Math.round((d[refuseType] / getPopulation(d)) * 2000)} pounds/year<br/><br/>per person: ${formatPoundsPerPerson((d[refuseType] / getPopulation(d)) * 2000)} pounds/year<br/><br/>    <p>Breakdown of refuse by percent:</p>
+    per person: ${Math.round((d[refuseType] / getPopulation(d)) * 2000)} pounds/year<br/><br/>
     <ul>${listItems}</ul>
   `;
 }
